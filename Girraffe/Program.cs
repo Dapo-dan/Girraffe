b2fefbc96514ed44c30319fc6f3b04a75c3b4186
Dapo <odapo138@gmail.com>
2024-05-08 23:47:40 +0100

    feat: completed C# tutorial

--- Girraffe/Program.cs
+++ Girraffe/Program.cs
@@ -4,19 +4,13 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Book book1 = new()
-        {
-            title = "Harry Porter",
-            author = "JK Rowling",
-            pages = 400,
-        };
+        Book book1 = new("JK Rowling", "Harry Porter", 400);
 
-        Book book2 = new()
-        {
-            title = "Lord Of The Rings",
-            author = "Tolkein",
-            pages = 700,
-        };
+        Book book2 = new(
+            aTitle: "Lord Of The Rings",
+            aAuthor: "Tolkein",
+            aPages: 700
+        );
 
         Console.WriteLine(book1.pages);
 
